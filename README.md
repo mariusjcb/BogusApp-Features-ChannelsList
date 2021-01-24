5011898a884f63064a16cc11ff4cdb33ef9f6fab
Marius Ilie <marius@iliemarius.ro>
2021-01-24 18:22:44 +0200

    Fix origin

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# BogusApp-Features-PlansList
+# BogusApp-Features-ChannelsList
 
 A description of this package.
--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,7 +1,7 @@
 import XCTest
 
-import BogusApp_Features_PlansListTests
+import BogusApp_Features_ChannelsListTests
 
 var tests = [XCTestCaseEntry]()
-tests += BogusApp_Features_PlansListTests.allTests()
+tests += BogusApp_Features_ChannelsListTests.allTests()
 XCTMain(tests)
